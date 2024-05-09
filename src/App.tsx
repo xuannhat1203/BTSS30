e106205854b4c10c7a32f26f4b902248eb47135a
xuannhat <xuannhatvn211@gmail.com>
2024-05-09 09:16:24 +0700

    BTSS30

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
-import BT01 from "./components/BT01";
-import BT02 from "./components/BT02";
-import BT03 from "./components/BT03";
-import BT04 from "./components/BT04";
-import BT05 from "./components/BT05";
-import BT06 from "./components/BT06";
-import BT08 from "./components/BT08";
+// App.tsx
+import React, { useState } from "react";
+import Jobs from "./components/Jobs";
+import "./BT08.css";
+
 export default function App() {
+  const [valueInput, setValueInput] = useState("");
+  const [jobs, setJobs] = useState(JSON.parse(localStorage.getItem("jobs") || "[]"));
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValueInput(e.target.value);
+  };
+
+  const handleAddJob = () => {
+    const newJob = {
+      name: valueInput,
+      id: Math.random() * 10000,
+      status: false,
+    };
+    const updatedJobs = [...jobs, newJob];
+    setJobs(updatedJobs);
+    setValueInput("");
+    localStorage.setItem("jobs", JSON.stringify(updatedJobs));
+  };
+
   return (
     <div>
-      <BT01 />
-      <BT02 />
-      <BT03 />
-      <BT04 />
-      <BT05 />
-      <BT06 />
-      <BT08 />
+      <h3 id="listJob">Danh sách công việc</h3>
+      <div id="inputButton">
+        <input
+          id="inputText"
+          value={valueInput}
+          placeholder="Nhập tên công việc"
+          onChange={handleInput}
+          type="text"
+          className="input-field"
+          style={{ color: "black" }}
+        />
+        <button id="addJob" onClick={handleAddJob}>
+          Thêm
+        </button>
+      </div>
+      <Jobs jobs={jobs} />
     </div>
-  )
+  );
 }
